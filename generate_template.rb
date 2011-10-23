f4158bd5fea3b9af51f57d2c4b732080f492f9ce
Strzelewicz Alexandre <strzelewicz.alexandre@gmail.com>
2011-10-24 01:00:28 +0200

    Fix

--- generate_template.rb
+++ generate_template.rb
@@ -1,22 +1,22 @@
 #
 #
-# Template gen with :
-# rails new eschool -m template/generate_template.rb
+# Rails_Template gen with :
+# rails new eschool -m rails_template/generate_rails_template.rb
 #
 
 run "rm public/index.html"
 
 
 # if yes?("Bootstrap stylesheet ?")
-#   run "cp -vr ../template/blueprint/app/assets/stylesheets/bootstrap.min.css app/assets/stylesheets/"
+#   run "cp -vr ../rails_template/blueprint/app/assets/stylesheets/bootstrap.min.css app/assets/stylesheets/"
 # end
 
 # if yes?("HTML5 Full complient Views & partials (and some other stuffs) ?")
-#   run "cp -vr ../template/blueprint/app/views/* app/views/"
-#   run "cp -vr ../template/blueprint/public/* public/"
+#   run "cp -vr ../rails_template/blueprint/app/views/* app/views/"
+#   run "cp -vr ../rails_template/blueprint/public/* public/"
 # end
 
-run "cp -vr ../template/blueprint/* ."
+run "cp -vr ../rails_template/blueprint/* ."
 
 generate(:controller, "home index about")
 
@@ -32,7 +32,7 @@ gem 'paperclip'
 if yes?("Would you like to install dynamic forms ?")
   gem 'dynamic_form', :git => 'git://github.com/Alexandre-Strzelewicz/dynamic_form.git'
 else
-  run "rm lib/templates/erb/scaffold/_form.html.erb"
+  run "rm lib/rails_template/erb/scaffold/_form.html.erb"
 end
 
 if yes?("Would you like to install Devise ?")
