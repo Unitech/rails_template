5e044d1a7b72e2680f7b4bb56fc23a48aeaa5b59
Strzelewicz Alexandre <strzelewicz.alexandre@gmail.com>
2011-10-25 11:19:59 +0200

    Notificator added

---
+++ blueprint/app/assets/javascripts/utils.js
@@ -0,0 +1,12 @@
+var rdy = 1;
+function notify(str, time){
+    if (rdy == 1){
+	rdy = 0;
+	time = typeof(time) != 'undefined' ? time : 2200;
+	var note = $('#notificator');
+	note.html(str);
+	note.slideDown().delay(time).slideUp(function(){rdy=1});
+    }
+    return false;
+}
+
