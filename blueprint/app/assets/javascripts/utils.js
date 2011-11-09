c6ad7ea102688f4c95b4ce023b297989beb03f49
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2011-11-10 00:45:04 +0100

    Js notificator, flash message modified

--- blueprint/app/assets/javascripts/utils.js
+++ blueprint/app/assets/javascripts/utils.js
@@ -1,14 +1,24 @@
-var rdy = 1;
+/*
+ * Some tools
+ */
 
-$().ready(function() {
-    function notify(str, time){
-	if (rdy == 1){
-	    rdy = 0;
-	    time = typeof(time) != 'undefined' ? time : 2200;
-	    var note = $('#notificator');
-	    note.html(str);
-	    note.slideDown().delay(time).slideUp(function(){rdy=1});
-	}
-	return false;
+Utils = {
+    notify : function(str, time) {
+        if (this.init == undefined) {
+            $('body').append('<div id="notificator"></div>');
+            this.init = true;
+            this.rdy = true;
+            this.el = $('#notificator');
+        }
+        if (this.rdy == true){
+            this.rdy = false;
+            var self = this;
+            time = typeof(time) != 'undefined' ? time : 2200;
+            this.el.html(str);
+            this.el.slideDown().delay(time).slideUp(function(){
+                self.rdy=1
+            });
+        }
+        return false;
     }
-});
+}
