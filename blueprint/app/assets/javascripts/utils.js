a1479fe7050e1b7910ef782e404004e24b5ece98
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2011-11-09 01:03:21 +0100

    Add some stuff

--- blueprint/app/assets/javascripts/utils.js
+++ blueprint/app/assets/javascripts/utils.js
@@ -1,4 +1,5 @@
 var rdy = 1;
+
 function notify(str, time){
     if (rdy == 1){
 	rdy = 0;
@@ -10,3 +11,18 @@ function notify(str, time){
     return false;
 }
 
+// var $rootDOM;
+// $rootDOM = $('#social-buttons-container');
+// $.getScript('http://connect.facebook.net/en_US/all.js#appId=111098168994577&amp;xfbml=1', function() {
+//   FB.init({
+//     appId: '111098168994577',
+//     xfbxml: true,
+//     status: true,
+//     cookie: true
+//   });
+//   return FB.XFBML.parse($rootDOM.get(0));
+// });
+// $.getScript('http://platform.twitter.com/widgets.js', function() {
+//   return $rootDOM.find('.twitter-share-button').show();
+// });
+// return $.getScript('https://apis.google.com/js/plusone.js');
