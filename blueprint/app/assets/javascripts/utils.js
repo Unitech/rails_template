2d7ae15c915d40a6176a623fd7286d98d871c060
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2011-11-09 22:57:51 +0100

    JS snippets added

--- blueprint/app/assets/javascripts/utils.js
+++ blueprint/app/assets/javascripts/utils.js
@@ -1,28 +1,14 @@
 var rdy = 1;
 
-function notify(str, time){
-    if (rdy == 1){
-	rdy = 0;
-	time = typeof(time) != 'undefined' ? time : 2200;
-	var note = $('#notificator');
-	note.html(str);
-	note.slideDown().delay(time).slideUp(function(){rdy=1});
+$().ready(function() {
+    function notify(str, time){
+	if (rdy == 1){
+	    rdy = 0;
+	    time = typeof(time) != 'undefined' ? time : 2200;
+	    var note = $('#notificator');
+	    note.html(str);
+	    note.slideDown().delay(time).slideUp(function(){rdy=1});
+	}
+	return false;
     }
-    return false;
-}
-
-// var $rootDOM;
-// $rootDOM = $('#social-buttons-container');
-// $.getScript('http://connect.facebook.net/en_US/all.js#appId=111098168994577&amp;xfbml=1', function() {
-//   FB.init({
-//     appId: '111098168994577',
-//     xfbxml: true,
-//     status: true,
-//     cookie: true
-//   });
-//   return FB.XFBML.parse($rootDOM.get(0));
-// });
-// $.getScript('http://platform.twitter.com/widgets.js', function() {
-//   return $rootDOM.find('.twitter-share-button').show();
-// });
-// return $.getScript('https://apis.google.com/js/plusone.js');
+});
